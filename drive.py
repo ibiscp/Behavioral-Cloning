cca76b428ed2e987df04d09e08c5124fff4f8405
ibiscp <ibiscp@gmail.com>
2017-02-13 06:45:48 -0200

    Problem after the dirty exit

--- drive.py
+++ drive.py
@@ -24,7 +24,6 @@ def cut_image(img):
     rows, cols, channel = img.shape
     top = int(.4 * rows)
     botton = int(.85 * rows)
-    border = int(.05 * cols)
     return img[top:botton, :, :]#border:cols-border, :]
 
 sio = socketio.Server()
@@ -41,23 +40,17 @@ def telemetry(sid, data):
     # The current speed of the car
     speed = data["speed"]
     # The current image from the center camera of the car
-    model_height = 64
-    model_weight = 64
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
 
-    # image_array = np.asarray(image)
-    # # image_cuted = cut_image(image_array)
-    # # print(image_array.shape)
-    #
-    # image_array = (image_array - 128.0) / 128.0
+    # Size of the image
+    model_height = 64
+    model_weight = 64
 
     image_cuted = cut_image(np.copy(image))
-    #image_array = (image_cuted - 128.0) / 128.0
     image_resized = cv2.resize(image_cuted,(model_height, model_weight), interpolation = cv2.INTER_CUBIC)
     image_array = np.reshape(image_resized, [1, model_height, model_weight, 3])
 
-    #transformed_image_array = image_array[None, :, :, :]
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(image_array, batch_size=1))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
